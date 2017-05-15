33e1d8b333b0048b533c1b0cc5b5bbae41cb118d
Ernesto R Espinoza <Ernesto@Ernestos-MacBook-Pro.local>
2017-05-14 19:37:07 -0700

    added more words to my word bank, still cannot make my javascript file reach as an external file

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -1,4 +1,4 @@
-<script type="text/javascript">
+
 
     var doubleWord = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
@@ -117,4 +117,3 @@ document.onkeyup = function(event) {
  
 }
 
-</script>
